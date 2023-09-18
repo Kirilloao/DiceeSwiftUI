cf4d1edfc1ce7ed57783a03f12c4bf6d13f3232f
Kirilloao <kirilltaraturin747@gmail>
2023-09-18 16:05:21 +0200

    Initial Commit

---
+++ DiceeSwiftUI/DiceeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeSwiftUIApp.swift
+//  DiceeSwiftUI
+//
+//  Created by Kirill Taraturin on 18.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct DiceeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
